b3555f9e530509f15715fe521a352da8700eaca2
GracieChristine <graciechristineli@gmail.com>
2017-07-04 21:42:02 -0600

    Error on Config.js, fix it later.

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -5,20 +5,35 @@ import {
   View,
   TextInput,
   Button,
+  Picker,
   TouchableOpacity
 } from "react-native";
 
+import Config from "../../config.js";
+
 export default class Translation extends Component{
-
-  static navigationOptions = {
-    title: 'Text Translation',
-  };
-
   constructor(props) {
     super(props);
     this.state = {
-
+      textInput: "Hello world",
+      textOutput: "",
+      langSource: "en",
+      langTarget: "en"
     };
+  }
+
+  tranaslateText(textInput, langSource, langTarget) {
+    // console.log("langSource", langSource);
+    fetch(`https://translation.googleapis.com/language/translate/v2?q=${textInput}&target=${langTarget}&format=text&source=${langSource}&key=${Config.googleAPIKey}`)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON.data.translations[0].translatedText);
+        const text = responseJSON.data.translations[0].translatedText;
+        this.setState({textOutput: text})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
@@ -27,6 +42,47 @@ export default class Translation extends Component{
         <View style={styles.titleContainer}>
           <Text style={styles.title}>
             Text-to-Text Translation
+          </Text>
+        </View>
+        <View style={styles.langContainer}>
+          <View  style={styles.eachContainer}>
+            <Text style={styles.pickerTitle}>Source Language</Text>
+            <Picker
+              style={styles.pickerMenu}
+              selectedValue={this.state.langSource}
+              onValueChange={(itemValue, itemIndex) => this.setState({langSource: itemValue})}>
+              <Picker.Item label="English" value="en" />
+              <Picker.Item label="Español" value="es" />
+              <Picker.Item label="國語" value="zh" />
+              <Picker.Item label="Français" value="fr" />
+            </Picker>
+          </View>
+          <View style={styles.eachContainer}>
+            <Text style={styles.pickerTitle}>Target Language</Text>
+            <Picker
+              style={styles.pickerMenu}
+              selectedValue={this.state.langTarget}
+              onValueChange={(itemValue, itemIndex) => this.setState({langTarget: itemValue})}>
+              <Picker.Item label="English" value="en" />
+              <Picker.Item label="Español" value="es" />
+              <Picker.Item label="國語" value="zh" />
+              <Picker.Item label="Français" value="fr" />
+            </Picker>
+          </View>
+        </View>
+        <View style={styles.translationContainer}>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={(textInput) => this.setState({textInput})}
+            keyboardType={"default"}
+          />
+          <TouchableOpacity style={styles.translationBtn}
+            onPress={() => this.tranaslateText(this.state.textInput, this.state.langSource, this.state.langTarget)}
+          >
+            <Text style={styles.translationBtnText}>Translate!</Text>
+          </TouchableOpacity>
+          <Text style={styles.outputText}>
+            {this.state.textOutput}
           </Text>
         </View>
       </View>
@@ -43,13 +99,75 @@ let styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    // backgroundColor: "lightgray",
     borderBottomColor: "#C59A6D"
   },
   title: {
     color: "#C59A6D",
     fontSize: 24,
     fontWeight: "700"
+  },
+  langContainer: {
+    flex: 3,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "flex-start"
+  },
+  eachContainer: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "stretch"
+  },
+  pickerTitle: {
+    marginTop: 50
+  },
+  pickerMenu: {
+    width: 100,
+    marginTop: -15
+  },
+  translationContainer: {
+    flex: 3,
+  },
+  inputText: {
+    width: 200,
+    height: 50,
+    marginLeft: 85,
+    marginTop: 15,
+    marginBottom: 15,
+    // borderRadius: 20,
+    padding: 5,
+    backgroundColor: "whitesmoke",
+  },
+  translationBtn: {
+    width: 110,
+    marginLeft: 130,
+    backgroundColor: "#C59A6D",
+    borderRadius: 10,
+    padding: 5,
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25
+  },
+  translationBtnText: {
+    color: "white",
+    fontSize: 18,
+    marginLeft: 10
+  },
+  outputText: {
+    width: 200,
+    height: 50,
+    marginLeft: 85,
+    marginTop: 15,
+    marginBottom: 15,
+    borderRadius: 50,
+    padding: 10,
+    paddingTop: 15,
+    fontSize: 18,
+    backgroundColor: "#4A90E2",
+    color: "white",
   }
 });
 
